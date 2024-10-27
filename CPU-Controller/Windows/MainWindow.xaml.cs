4e3f0ee45b2592d8ea7b7e172557efcd4a9db9c4
BloodSharp <racconrolly@gmail.com>
2024-10-27 08:52:52 +0300

    Добавьте файлы проекта.

---
+++ CPU-Controller/Windows/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CPU_Controller.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
