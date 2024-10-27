4ffc9d323d45e4177013df704077443fd3a48813
BloodSharp <racconrolly@gmail.com>
2024-10-27 13:43:19 +0300

    Интерфейс сделан

--- CPU-Controller/Windows/MainWindow.xaml.cs
+++ CPU-Controller/Windows/MainWindow.xaml.cs
@@ -12,12 +12,29 @@ namespace CPU_Controller.Windows
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
-
+        bool IsMaximized = false;
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1280;
+                    this.Height = 780;
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+                }
+            }
         }
     }
 }
