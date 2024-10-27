96ec0598f5ba8ab1627da28bcdaf402a858ef54b
BloodSharp <racconrolly@gmail.com>
2024-10-27 15:39:47 +0300

    Classes/Pages

---
+++ CPU-Controller/Pages/CPUPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPU_Controller.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CPUPage.xaml
+    /// </summary>
+    public partial class CPUPage : Page
+    {
+        public CPUPage()
+        {
+            InitializeComponent();
+        }
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(CPUPage));
+
+
+        public string SubTitle
+        {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubTitleProperty = DependencyProperty.Register("SubTitle", typeof(string), typeof(CPUPage));
+
+
+        public int Amount
+        {
+            get { return (int)GetValue(AmountProperty); }
+            set { SetValue(AmountProperty, value); }
+        }
+
+        public static readonly DependencyProperty AmountProperty = DependencyProperty.Register("Amount", typeof(int), typeof(CPUPage));
+
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(CPUPage));
+
+    }
+}
+
--- CPU-Controller/Windows/MainWindow.xaml.cs
+++ CPU-Controller/Windows/MainWindow.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Windows;
+﻿using CPU_Controller.Pages;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Xml.Linq;
 
 namespace CPU_Controller.Windows
 {
@@ -35,6 +41,70 @@ namespace CPU_Controller.Windows
                     IsMaximized = true;
                 }
             }
+        }
+
+        private void ButtonCPU_Click(object sender, RoutedEventArgs e)
+        {
+            FrameoutputInfo.Navigate(new CPUPage());
+        }
+
+        private void ButtonGPU_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonRAM_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonROM_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonBIOS_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("shutdown", "/r /fw /t 0");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void ButtonRegistryOP_Click(object sender, RoutedEventArgs e)
+        {
+            //foreach (string subKeyName in key.GetSubKeyNames())
+            //{
+            //    using (RegistryKey subKey = key.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\"))
+            //    {
+            //        if (subKey == null)
+            //        {
+            //            continue; // Пропускаем ключ, если у нас нет доступа к нему
+            //        }
+
+            //        Paragraph keyParagraph = parentRange.Paragraphs.Add();
+            //        keyParagraph.Range.Text = $"Ключ: {subKeyName}";
+            //        keyParagraph.Range.InsertParagraphAfter();
+
+            //        foreach (string valueName in subKey.GetValueNames())
+            //        {
+            //            object value = subKey.GetValue(valueName);
+            //            Paragraph valueParagraph = parentRange.Paragraphs.Add();
+            //            valueParagraph.Range.Text = $"\t{valueName}: {value}";
+            //            valueParagraph.Range.InsertParagraphAfter();
+            //        }
+
+            //        TraverseRegistry(subKey, parentRange); // Рекурсивный обход подключей
+            //    }
+            //}
+        }
+        private void ButtonALL_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonContact_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
